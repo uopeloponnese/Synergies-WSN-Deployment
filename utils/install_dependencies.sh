6b428d76c061d3aff93ff2f1a30e47d826fc78c5
sotiriostsak <tsakalidis.sotirios@gmail.com>
2023-07-12 19:39:00 +0300

    install_dependencies.sh docker and docker-compose

--- utils/install_dependencies.sh
+++ utils/install_dependencies.sh
@@ -3,6 +3,62 @@ echo "Installing dependencies..."
 
 # Update repositories
 sudo apt-get update
+sudo apt-get -y upgrade
 
-# Install OpenVPN (if not already installed)
+##### OVPN #####
 sudo apt-get install openvpn -y
+
+# Check if OpenVPN is installed
+if ! command -v openvpn &>/dev/null; then
+    echo "OpenVPN installation failed. Please check the installation steps and try again."
+    sleep 2
+    exit 1
+else
+    echo "OpenVPN installation successful."
+fi
+
+##### Docker #####
+# uninstall all conflicting packages
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Set up the repository
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+# Add Docker’s official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Use the following command to set up the repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# Check if Docker is installed
+if ! command -v docker &>/dev/null; then
+    echo "Docker installation failed. Please check the installation steps and try again."
+    sleep 2
+    exit 1
+else
+    echo "Docker installation successful. Version: $(docker --version)"
+fi
+
+##### Docker-Compose #####
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &>/dev/null; then
+    echo "Docker Compose installation failed. Please check the installation steps and try again."
+    sleep 2
+    exit 1
+else
+    echo "Docker Compose installation successful. Version: $(docker-compose --version)"
+fi
