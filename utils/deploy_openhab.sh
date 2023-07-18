10eb66ae4b74e3291c14e5076b21ee62d9ab5829
sotiriostsak <tsakalidis.sotirios@gmail.com>
2023-07-18 15:00:13 +0300

    "openHAB started!" - fix

--- utils/deploy_openhab.sh
+++ utils/deploy_openhab.sh
@@ -4,9 +4,15 @@ echo "Setting up openHAB..."
 sudo docker-compose --env-file ../docker.env up -d
 
 # Wait for OpenHAB to start
-while [[ $(sudo docker exec -it openhab /openhab/runtime/bin/status) != "Running ..." ]]; do
-    sleep 5
-    echo "Not started yet..."
+while true; do
+    status=$(sudo docker exec -it openhab /openhab/runtime/bin/status)
+    if echo "$status" | grep -q "Running ..."; then
+        echo "OpenHAB is running."
+        break
+    else
+        echo "OpenHAB is not running yet. Waiting..."
+        sleep 5
+    fi
 done
 
 # Continue with the rest of your deployment steps
