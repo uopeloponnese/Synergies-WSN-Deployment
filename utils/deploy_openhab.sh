0c10418aefb9dee690ec97194e5f52f8ed9527e5
sotiriostsak <tsakalidis.sotirios@gmail.com>
2023-07-19 11:44:05 +0300

    Check if Z-Wave controller is connected and /dev/ttyACM0 is available

--- utils/deploy_openhab.sh
+++ utils/deploy_openhab.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 echo "Setting up openHAB..."
 
+# Check if Z-Wave controller is connected and /dev/ttyACM0 is available
+while true
+do
+  if ! lsusb | grep -q "Aeotec"; then
+    echo "Z-Wave controller not detected. Please connect the Aeotec Z-Stick Gen5+ Z-Wave controller and press Enter to continue ..."
+  else
+    echo "Z-Wave controller connected."
+    if [ ! -e /dev/ttyACM0 ]; then
+      echo "/dev/ttyACM0 device not found. Please contact the UoP WSN Technical Consultant."
+      exit 1
+    fi
+    break
+  fi
+
+  read -p ""
+done
+
+echo "Continue with the rest of the deployment process ..."
+
+# Running docker compose
 sudo docker-compose --env-file ../docker.env up -d
 
 # Wait for OpenHAB to start
