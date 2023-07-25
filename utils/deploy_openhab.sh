b9718977a60dedfccda7fed5569b37b60dd3ace2
sotiriostsak <tsakalidis.sotirios@gmail.com>
2023-07-25 20:11:38 +0300

    openhab_services directory - fix

--- utils/deploy_openhab.sh
+++ utils/deploy_openhab.sh
@@ -40,12 +40,12 @@ while true; do
     fi
 done
 
-# Enable console
-sudo docker exec openhab sed -i 's/^#org.apache.karaf.shell:sshHost = 0.0.0.0/org.apache.karaf.shell:sshHost = 0.0.0.0/' /openhab/conf/services/runtime.cfg
-echo "openHAB console enabled."
-
-sudo docker restart openhab
-echo "openHAB is restarting..."
+## Enable console - No need for manual enabling after mounting /openhab/conf/services
+#sudo docker exec openhab sed -i 's/^#org.apache.karaf.shell:sshHost = 0.0.0.0/org.apache.karaf.shell:sshHost = 0.0.0.0/' /openhab/conf/services/runtime.cfg
+#echo "openHAB console enabled."
+#
+#sudo docker restart openhab
+#echo "openHAB is restarting..."
 
 
 # Continue with the rest of your deployment steps
