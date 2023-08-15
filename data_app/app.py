c6c4aeac262a4fc02145d2bd2386bbdbea7d300d
sotiriostsak <tsakalidis.sotirios@gmail.com>
2023-08-15 19:14:53 +0300

    data-app in docker compose - fix 5

--- data_app/app.py
+++ data_app/app.py
@@ -5,19 +5,18 @@ from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
-
-
-@api.representation('application/json')
-def output_json(data, code=None, headers=None):
-    json_response = json.dumps(data, indent=4)
-    return Response(json_response, status=200, mimetype='application/json')
-
+#
+#
+# @api.representation('application/json')
+# def output_json(data, code=None, headers=None):
+#     json_response = json.dumps(data, indent=4)
+#     return Response(json_response, status=200, mimetype='application/json')
+#
 
 # endpoints
 # api.add_resource(rest.Greet, '/greet', resource_class_kwargs={'representations': {'application/json': output_json}})
-api.add_resource(rest.Greet, '/hello')
+api.add_resource(rest.Greet, '/greet')
 
 if __name__ == "__main__":
-    # ekkinisi tou server se ola ta interfaces tou, me default port tin 5000
     app.run(host="0.0.0.0")  # default port 5000
     # app.run(host="0.0.0.0", ssl_context=('ssl/certificate.crt', 'ssl/private.key'))  # default port 5000
